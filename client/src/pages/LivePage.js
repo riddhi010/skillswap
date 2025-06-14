95e34f7d2238740d38173e21c1c542500e705389
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-06-14 17:00:50 +0530

    Create LivePage.js

---
+++ client/src/pages/LivePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import LiveSession from "./LiveSession";
+
+const LivePage = () => {
+  const { username, roomId } = useParams();
+
+  return <LiveSession username={username} roomId={roomId} />;
+};
+
+export default LivePage;
