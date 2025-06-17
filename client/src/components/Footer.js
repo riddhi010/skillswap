efa7d77ea1563fef590765222e10cd2f68ac295b
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-06-17 21:06:12 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => (
   <footer className="p-6 text-center text-gray-300 bg-white/10 backdrop-blur-md border-t border-white/20">
-    &copy; {new Date().getFullYear()} SkillSwap. Built with ❤️ by Riddhi.
+    &copy; {new Date().getFullYear()} SkillSwap.
   </footer>
 );
 
