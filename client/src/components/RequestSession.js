c28b6cec67d64917d6ae00861f8e8d28f4de8a92
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-06-30 23:46:46 +0530

    Create RequestSession.js

---
+++ client/src/components/RequestSession.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const RequestSession = ({ currentUserId, peerId }) => {
+  const [skill, setSkill] = useState("");
+  const [time, setTime] = useState("");
+
+  const requestSession = async () => {
+    const res = await axios.post("http://localhost:5000/api/sessions", {
+      skill,
+      requesterId: currentUserId,
+      responderId: peerId,
+      scheduledTime: new Date(time)
+    });
+    alert("Session Requested Successfully");
+  };
+
+  return (
+    <div>
+      <input placeholder="Skill" value={skill} onChange={e => setSkill(e.target.value)} />
+      <input type="datetime-local" value={time} onChange={e => setTime(e.target.value)} />
+      <button onClick={requestSession}>Request Session</button>
+    </div>
+  );
+};
+
+export default RequestSession;
