b694d0dcb8b62f8e6f2d452dd1051a884e111eed
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-07-01 10:35:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/register";
 import Login from "./pages/login";
 import Dashboard from "./components/Dashboard";
 import ExploreUsers from "./components/ExploreUsers"
 import Profile from "./components/Profile";
-
-import Home from "./pages/home";
-
 import LiveSession from "./components/LiveSession";
+import Home from "./pages/home";
+import Loader from './components/Loader';
+
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3600); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
   return (
+  <>
+    {loading ? (
+        <Loader />
+      ) : (
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
@@ -18,12 +35,13 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/explore" element={<ExploreUsers />} />
         <Route path="/profile" element={<Profile />} />
-        
         <Route path="/live" element={<LiveSession />} />
         <Route path="/" element={<Home />} />
-        {/* Add more routes here */}
+       
       </Routes>
     </Router>
+     )}
+    </>
   );
 }
 
