131d62ce4c10efc3bfd3cc3563a94fb9ac276fcb
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-07-14 14:57:56 +0530

    Update LiveSession.js

--- client/src/components/LiveSession.js
+++ client/src/components/LiveSession.js
@@ -65,8 +65,18 @@ const LiveSession = () => {
 
  const createPeerConnection = () => {
   peerRef.current = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+    iceServers: [
+      {
+        urls: "stun:stun.l.google.com:19302"
+      },
+      {
+        urls: "turn:relay1.expressturn.com:3478",
+        username: "efJQy92g1Zyb7VNaYq7Z8g==",
+        credential: "kF1Wh5H8iIWNoYV1"
+      }
+    ]
   });
+
 
   peerRef.current.onicecandidate = (event) => {
     if (event.candidate) {
