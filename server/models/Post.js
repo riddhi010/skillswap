e2c0ddca59db85a1849ea455cb30b83508c0dea4
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-07-18 14:29:58 +0530

    Add files via upload

---
+++ server/models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  comments: [commentSchema],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Post", postSchema);
