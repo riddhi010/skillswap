3fcb99e1a848651cc3be99572413daa9fdf0ac72
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-07-22 00:58:46 +0530

    Update session.js

--- server/models/session.js
+++ server/models/session.js
@@ -4,13 +4,15 @@ const sessionSchema = new mongoose.Schema({
   learner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   mentor: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   skill: { type: String, required: true },
+  message:String,
   scheduledAt: { type: Date, required: true },
   status: {
     type: String,
-    enum: ["pending", "approved", "rejected", "completed"],
+    enum: ["pending", "accepted", "rejected"],
     default: "pending",
   },
-  message: { type: String }, // optional message from learner
+  meetingId: { type: String, default: "" }, 
+
 }, { timestamps: true });
 
 module.exports = mongoose.model("Session", sessionSchema);
