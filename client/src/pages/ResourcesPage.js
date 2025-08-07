d0d2ec26719f14456bd71570a9ce9a3d8c3667b8
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-08-07 15:47:45 +0530

    Update ResourcesPage.js

--- client/src/pages/ResourcesPage.js
+++ client/src/pages/ResourcesPage.js
@@ -38,7 +38,7 @@ const ResourcesPage = () => {
         onChange={(e) => setFilter(e.target.value.trim())}
       />
 
-      {/* List */}
+      
       {/* List */}
 <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
   {resources.map((r) => (
@@ -86,3 +86,4 @@ const ResourcesPage = () => {
 };
 
 export default ResourcesPage;
+
